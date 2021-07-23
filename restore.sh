ca325344e823ca12fff39899635c15cac1a1525a
Altair-Bueno <altair.bueno@uma.es>
2021-07-23 21:34:11 +0200

    Updated restore

--- restore.sh
+++ restore.sh
@@ -3,11 +3,11 @@ echo "This script will only setup the simlinks required for \
 config files. Yoy need to manualy install the tools using brew, \
 omz, etc. See README.md"
 
-export BACKUP=$HOME/.backup/
-
-mkdir $BACKUP
-ln -s * $BACKUP
 cd $HOME
+git clone https://github.com/Altair-Bueno/Demeter
+ln -s Demeter/* $HOME
 ls -la
+
+echo "Done"
 
 exit 0 
