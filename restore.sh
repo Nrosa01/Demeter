38a3029eea74b06d8b6d68fbc6533c47749202cd
Altair-Bueno <altair.bueno@uma.es>
2021-07-23 22:01:12 +0200

    improved restore. Detects fails

--- restore.sh
+++ restore.sh
@@ -3,9 +3,11 @@ echo "This script will only setup the symlinks required for \
 config files. You need to manually install the tools using brew, \
 omz, etc. See README.md"
 
+# Move to Home dir
 cd $HOME
 
-if [ -z $GITHUB_WORKSPACE ]
+# Check CI
+if [ -z $GITHUB_WORKSPACE ];
 then
 	# Restoring backup
 	git clone https://github.com/Altair-Bueno/Demeter
@@ -15,9 +17,18 @@ else
 	export DEMETER=$GITHUB_WORKSPACE
 fi
 
+# Create links
 ln -s $DEMETER/* $HOME
-ls -la
 
-echo "Done"
+# Successfull
+if ls -la | grep "README.md ->";
+then
+	echo "Done"
+	exit 0 
+else
+	echo "Err"
+	exit 1	
+fi	
 
-exit 0 
+
+
