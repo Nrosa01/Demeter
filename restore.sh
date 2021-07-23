45b14c043b9a3c9ae13fe5f73f665be4b04efb12
Altair-Bueno <altair.bueno@uma.es>
2021-07-23 21:53:50 +0200

    CI added

--- restore.sh
+++ restore.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-echo "This script will only setup the simlinks required for \
-config files. Yoy need to manualy install the tools using brew, \
+echo "This script will only setup the symlinks required for \
+config files. You need to manually install the tools using brew, \
 omz, etc. See README.md"
 
 cd $HOME
-git clone https://github.com/Altair-Bueno/Demeter
-ln -s Demeter/* $HOME
+
+if [ -z $GITHUB_WORKSPACE ]
+then
+	# Restoring backup
+	git clone https://github.com/Altair-Bueno/Demeter
+	export DEMETER=$HOME/Demeter
+else 
+	# If CI workflow
+	export DEMETER=$GITHUB_WORKSPACE
+fi
+
+ln -s $DEMETER/* $HOME
 ls -la
 
 echo "Done"
